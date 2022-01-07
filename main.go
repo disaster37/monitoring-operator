ce88dc2112514f43299955fb82f446d5ced48637
disaster37 <linuxworkgroup@hotmail.com>
2022-01-07 13:31:47 +0000

    fix: Add route sheme on main to look if fix acctest on microshift
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- main.go
+++ main.go
@@ -24,6 +24,14 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	"github.com/disaster37/go-centreon-rest/v21"
+	monitorv1alpha1 "github.com/disaster37/monitoring-operator/api/v1alpha1"
+	"github.com/disaster37/monitoring-operator/controllers"
+	"github.com/disaster37/monitoring-operator/pkg/centreonhandler"
+	"github.com/disaster37/monitoring-operator/pkg/helpers"
+	routev1 "github.com/openshift/api/route/v1"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -31,14 +39,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	"github.com/disaster37/go-centreon-rest/v21"
-	monitorv1alpha1 "github.com/disaster37/monitoring-operator/api/v1alpha1"
-	"github.com/disaster37/monitoring-operator/controllers"
-	"github.com/disaster37/monitoring-operator/pkg/centreonhandler"
-	"github.com/disaster37/monitoring-operator/pkg/helpers"
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -57,6 +57,8 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(monitorv1alpha1.AddToScheme(scheme))
+
+	utilruntime.Must(routev1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
