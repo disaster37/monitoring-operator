58375290c09b6ef1cb9713b875c861aecbbd7527
disaster37 <linuxworkgroup@hotmail.com>
2022-08-29 12:26:38 +0000

    fix: Add test on api package
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- api/v1/centreonservice_test.go
+++ api/v1/centreonservice_test.go
@@ -3,6 +3,8 @@ package v1
 import (
 	"testing"
 
+	"github.com/disaster37/go-centreon-rest/v21/models"
+	"github.com/disaster37/monitoring-operator/pkg/centreonhandler"
 	"github.com/disaster37/monitoring-operator/pkg/helpers"
 	"github.com/stretchr/testify/assert"
 
@@ -93,4 +95,56 @@ func TestCentreonServiceIsValid(t *testing.T) {
 
 	centreonService = &CentreonService{}
 	assert.False(t, centreonService.IsValid())
+}
+
+func TestToCentreonService(t *testing.T) {
+
+	s := &CentreonService{
+		Spec: CentreonServiceSpec{
+			Host:     "host1",
+			Name:     "s1",
+			Template: "template1",
+			Groups:   []string{"group1"},
+			Macros: map[string]string{
+				"MAC1": "value1",
+			},
+			Arguments:           []string{"arg1"},
+			Categories:          []string{"cat1"},
+			CheckCommand:        "check",
+			NormalCheckInterval: "1s",
+			RetryCheckInterval:  "2s",
+			MaxCheckAttempts:    "3s",
+			ActiveCheckEnabled:  nil,
+			PassiveCheckEnabled: nil,
+			Activated:           true,
+		},
+	}
+
+	expectedS := &centreonhandler.CentreonService{
+		Host:                "host1",
+		Name:                "s1",
+		CheckCommand:        "check",
+		CheckCommandArgs:    "!arg1",
+		NormalCheckInterval: "1s",
+		RetryCheckInterval:  "2s",
+		MaxCheckAttempts:    "3s",
+		ActiveCheckEnabled:  "2",
+		PassiveCheckEnabled: "2",
+		Template:            "template1",
+		Groups:              []string{"group1"},
+		Categories:          []string{"cat1"},
+		Macros: []*models.Macro{
+			{
+				Name:       "MAC1",
+				Value:      "value1",
+				IsPassword: "0",
+			},
+		},
+		Activated: "1",
+		Comment:   "Managed by monitoring-operator",
+	}
+
+	currentS, err := s.ToCentreonService()
+	assert.NoError(t, err)
+	assert.Equal(t, expectedS, currentS)
 }
