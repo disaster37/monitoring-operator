bbcc6bae95c974d26f7a39124be4d43719995c6d
disaster37 <linuxworkgroup@hotmail.com>
2025-12-01 15:59:16 +0000

    fix: fix ci
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- ci/dagger/main.go
+++ ci/dagger/main.go
@@ -27,8 +27,8 @@ import (
 
 const (
 	kubeVersion                 = "1.31.0"
-	sdkVersion                  = "v1.37.0"
-	controllerGenVersion        = "v0.16.1"
+	sdkVersion                  = "v1.39.2"
+	controllerGenVersion        = "v0.18.0"
 	kustomizeVersion            = "v5.4.3"
 	cleanCrdVersion             = "v0.1.9"
 	opmVersion                  = "v1.48.0"
@@ -289,6 +289,9 @@ func (h *MonitoringOperator) CI(
 			if _, err = h.OperatorSDK.Oci().PublishCatalog(ctx, fmt.Sprintf("%s:latest", catalogName)); err != nil {
 				return nil, errors.Wrap(err, "Error when publish the latest catalog image")
 			}
+
+			dir = dir.WithNewFile("VERSION", version)
+			gitBranch = defaultBranch
 		}
 
 		if !isTag {
