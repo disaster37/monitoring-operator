ebf4cbe8d5d2881e0a00299af02c61bc63f02188
disaster37 <linuxworkgroup@hotmail.com>
2025-12-01 15:53:26 +0000

    fix: fix  tools version on ci
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- ci/dagger/main.go
+++ ci/dagger/main.go
@@ -32,6 +32,7 @@ const (
 	kustomizeVersion            = "v5.4.3"
 	cleanCrdVersion             = "v0.1.9"
 	opmVersion                  = "v1.48.0"
+	dockerVersion               = "28"
 	registry                    = "quay.io"
 	repository                  = "webcenter/monitoring-operator"
 	gitUsername          string = "github"
@@ -54,8 +55,19 @@ func New(
 	src *dagger.Directory,
 ) *MonitoringOperator {
 	return &MonitoringOperator{
-		Src:         src,
-		OperatorSDK: dag.OperatorSDK(src.WithoutDirectory("ci"), name),
+		Src: src,
+		OperatorSDK: dag.OperatorSDK(
+			src.WithoutDirectory("ci"),
+			name,
+			dagger.OperatorSDKOpts{
+				SDKVersion:           sdkVersion,
+				OpmVersion:           opmVersion,
+				ControllerGenVersion: controllerGenVersion,
+				CleanCrdVersion:      cleanCrdVersion,
+				KustomizeVersion:     kustomizeVersion,
+				DockerVersion:        dockerVersion,
+			},
+		),
 	}
 }
 
